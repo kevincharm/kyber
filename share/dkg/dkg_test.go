f6bff457a7641d9e4899701ad56d1743d55006ad
Kevin Charm <kevin@kevincharm.com>
2024-02-10 17:37:49 +0100

    bump commit

--- share/dkg/dkg_test.go
+++ share/dkg/dkg_test.go
@@ -1092,14 +1092,14 @@ func TestMinimumT(t *testing.T) {
 		input  int
 		output int
 	}{
-		{10, 6},
-		{6, 4},
-		{4, 3},
-		{3, 2},
-		{2, 2},
-		{7, 4},
-		{8, 5},
-		{9, 5},
+		{10, 1},
+		{6, 1},
+		{4, 1},
+		{3, 1},
+		{2, 1},
+		{7, 1},
+		{8, 1},
+		{9, 1},
 	}
 	for _, test := range tests {
 		in := test.input
