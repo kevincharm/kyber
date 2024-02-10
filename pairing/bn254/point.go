39c74fd86557d7e45983c2a4407d4d5b59c25ee7
Kevin Charm <kevin@kevincharm.com>
2024-02-10 20:30:35 +0100

    fix: actually write bytes in zeroPadBytes

--- pairing/bn254/point.go
+++ pairing/bn254/point.go
@@ -376,6 +376,7 @@ func zeroPadBytes(m []byte, outlen int) []byte {
 	if len(m) < outlen {
 		padlen := outlen - len(m)
 		out := bytes.NewBuffer(make([]byte, padlen, outlen))
+		out.Write(m)
 		return out.Bytes()
 	}
 	return m
